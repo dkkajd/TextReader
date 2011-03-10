18b8776889d19898e8799e4b5a67bf83342d90c8
dkkajf_cp <SND\dkkajf_cp>
2011-03-10 12:26:12 +0000

    First upload to codeplex after having worked on it a few days.

---
+++ TextReader/TextReader/MainWindow.xaml.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Speech.Synthesis;
+using System.Text.RegularExpressions;
+using System.ComponentModel;
+using System.Diagnostics;
+
+namespace TextReader
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private DocumentReader _reader;
+
+        Dictionary<ReaderState, String> StateToText = new Dictionary<ReaderState, string>{
+                    {ReaderState.NotSpeaking, "Stopped"},
+                    {ReaderState.Paused, "Paused"},
+                    {ReaderState.Speaking, "Speaking"},
+                    {ReaderState.StartedSpeaking, "Started speaking"}};
+
+    //    public static RoutedCommand RemoveDblCommand = new RoutedCommand();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            _reader = new DocumentReader();
+            _reader.PropertyChanged += new PropertyChangedEventHandler(_reader_PropertyChanged);
+
+            this.DataContext = _reader;
+
+            comboBox1.ItemsSource=_reader.GetVoices();
+
+            // set the fields
+            txtStatus.Content = StateToText[_reader.State];
+            sldSpeakSpeed.Value = _reader.GetRate();
+            comboBox1.SelectedItem = _reader.GetVoice();
+
+            // Add commandbindings
+
+            CommandBinding PlayCmdBinding = new CommandBinding(
+                MediaCommands.Play, PlayCmdExecuted, PlayCmdCanExecute);
+            this.CommandBindings.Add(PlayCmdBinding);
+
+            CommandBinding PauseCmdBinding = new CommandBinding(
+                MediaCommands.Pause, PauseCmdExecuted, PauseCanExecute);
+            this.CommandBindings.Add(PauseCmdBinding);
+
+            CommandBinding StopCmdBinding = new CommandBinding(
+                MediaCommands.Stop, StopCmdExecuted, CanExecuteTrue);
+            this.CommandBindings.Add(StopCmdBinding);
+
+            CommandBinding RemoveDblCmdBinding = new CommandBinding(
+                (RoutedCommand)Resources["RemoveDblCommand"], RemoveDblCmdExecuted, CanExecuteTrue);
+            this.CommandBindings.Add(RemoveDblCmdBinding);
+
+            CommandBinding HomeCmdBinding = new CommandBinding(
+                (RoutedCommand)Resources["HomeCommand"], HomeCmdExecuted, CanExecuteTrue);
+            this.CommandBindings.Add(HomeCmdBinding);
+
+            CommandBinding EndCmdBinding = new CommandBinding(
+                (RoutedCommand)Resources["EndCommand"], EndCmdExecuted, CanExecuteTrue);
+            this.CommandBindings.Add(EndCmdBinding);
+        }
+
+        void CanExecuteTrue(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        void PlayCmdExecuted(object target, ExecutedRoutedEventArgs e)
+        {
+            _reader.StartReading(rtb.Selection.Start.GetLineStartPosition(0));
+            lastWord = _reader.ReadText;
+        }
+        void PlayCmdCanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = _reader.State == ReaderState.NotSpeaking;
+        }
+        void PauseCmdExecuted(object target, ExecutedRoutedEventArgs e)
+        {
+            switch (_reader.State)
+            {   case ReaderState.Speaking:
+                    _reader.PauseReading();
+                    break;
+                case ReaderState.Paused:
+                    _reader.ResumeReading();
+                    break;
+            }
+        }
+        void PauseCanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = _reader.State == ReaderState.Speaking ||
+                _reader.State == ReaderState.Paused;
+        }
+        void StopCmdExecuted(object target, ExecutedRoutedEventArgs e)
+        {
+            _reader.StopReading();
+        }
+        void RemoveDblCmdExecuted(object target, ExecutedRoutedEventArgs e)
+        {
+            var blocksToRemove = new List<Block>();
+            foreach (var block in rtb.Document.Blocks)
+            {
+                var para = block as Paragraph;
+                if (para != null && para.Inlines.Count == 1)
+                {
+                    var run = para.Inlines.FirstInline as Run;
+                    if (run != null && run.Text == "")
+                    {
+                        blocksToRemove.Add(block);
+                    }
+                }
+            }
+
+            foreach (var block in blocksToRemove)
+            {
+                rtb.Document.Blocks.Remove(block);
+            }
+        }
+        void HomeCmdExecuted(object target, ExecutedRoutedEventArgs e)
+        {
+            rtb.Selection.Select(rtb.Document.ContentStart, rtb.Document.ContentStart);
+        }
+        void EndCmdExecuted(object target, ExecutedRoutedEventArgs e)
+        {
+            rtb.Selection.Select(rtb.Document.ContentEnd, rtb.Document.ContentEnd);
+        }
+        
+        
+        TextRange lastWord;
+        void _reader_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            var inst = (DocumentReader)sender;
+            if (e.PropertyName=="ReadText")
+            {
+                unindicateWord();
+                TextPointer start = inst.ReadText.Start;
+                TextPointer end = inst.ReadText.End;
+
+                TextRange lastParagraph = new TextRange(inst.ReadText.Start.Paragraph.ContentStart, end.Paragraph.ContentEnd);
+                lastParagraph.ApplyPropertyValue(TextElement.BackgroundProperty, Brushes.Yellow);
+
+                rtb.Selection.Select(start, start);
+                lastWord = new TextRange(start, end);
+                lastWord.ApplyPropertyValue(TextElement.BackgroundProperty, Brushes.LightGreen);
+
+                Rect rect = start.GetCharacterRect(LogicalDirection.Forward);
+                if (sv != null && rect != null)
+                {
+                    rect.Y -= sv.ActualHeight / 2 - rect.Height / 2;
+                    sv.ScrollToVerticalOffset(rect.Y);
+                }
+                
+            }
+            else if (e.PropertyName == "State")
+            {
+                txtStatus.Content = StateToText[inst.State];
+                if (_reader.State == ReaderState.NotSpeaking)
+                {
+                    unindicateWord();
+                }
+            }
+            else if (e.PropertyName == "Rate")
+            {
+                sldSpeakSpeed.Value = _reader.GetRate();
+            }
+            this.UpdateLayout();
+        }
+
+        private void unindicateWord()
+        {
+            if (lastWord == null)
+                return;
+
+            // not needed, removing it with the paragraph
+            //// unindicated the last word
+            //lastWord.ApplyPropertyValue(TextElement.BackgroundProperty, Brushes.White);
+            
+            TextRange lastParagraph = new TextRange(lastWord.Start.Paragraph.ContentStart, lastWord.End.Paragraph.ContentEnd);
+            if (lastParagraph != null)
+                lastParagraph.ApplyPropertyValue(TextElement.BackgroundProperty, Brushes.White);
+        }
+
+        private void comboBox1_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            comboBox1.SelectedItem = _reader.SetVoice((String)comboBox1.SelectedItem );
+        }
+
+        private void sldSpeakSpeed_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            _reader.SetRate((int)e.NewValue);
+        }
+
+    }
+}
---
+++ TextReader/TextReader/Model/DocumentReader.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Controls;
+using System.Speech.Synthesis;
+using System.Windows.Documents;
+using System.Windows;
+using System.Collections.ObjectModel;
+using System.Text.RegularExpressions;
+using System.Windows.Media;
+using System.Windows.Controls.Primitives;
+using System.ComponentModel;
+
+namespace TextReader
+{
+    /// <summary>
+    /// The class the controls all the reading from a selected RichTextBox
+    /// </summary>
+    class DocumentReader : INotifyPropertyChanged
+    {
+
+        private SpeechSynthesizer _synth;
+
+        public DocumentReader()
+        {
+            State = ReaderState.NotSpeaking;
+            _synth = new SpeechSynthesizer();
+            _synth.SpeakProgress += new EventHandler<SpeakProgressEventArgs>(_synth_SpeakProgress);
+            _synth.SpeakCompleted += new EventHandler<SpeakCompletedEventArgs>(_synth_SpeakCompleted);
+            _synth.SpeakStarted += new EventHandler<SpeakStartedEventArgs>(_synth_SpeakStarted);
+        }
+
+        void _synth_SpeakStarted(object sender, SpeakStartedEventArgs e)
+        {
+            State = ReaderState.Speaking;
+        }
+
+        void _synth_SpeakCompleted(object sender, SpeakCompletedEventArgs e)
+        {
+            State = ReaderState.NotSpeaking;
+        }
+
+        void _synth_SpeakProgress(object sender, SpeakProgressEventArgs e)
+        {
+            // undo colors
+          //  unindicateWord();
+
+            var newPos = GetPositionAtTextOffset(startedReading,e.CharacterPosition);
+
+            if (newPos == null)
+            {
+                ReadText = null;
+            }
+            //    return;
+            
+            // indicate the word read
+            var end =GetPositionAtTextOffset( newPos,e.Text.Length);
+            if (end == null)
+                end = newPos;
+
+            ReadText = new TextRange(newPos, end);
+        }
+
+        static TextPointer GetPositionAtTextOffset(TextPointer source, int count)
+        {
+            var res = source;
+            var lastPara = res.Paragraph;
+
+
+            TextPointer lastPoint = null;
+            while (res != null && count > 0)
+            {
+                if (lastPoint != null && lastPoint.CompareTo(res) == 0)
+                    break;
+                lastPoint = res;
+                int length = res.GetTextRunLength(LogicalDirection.Forward);
+                if (count <= length)
+                {
+                    res = res.GetPositionAtOffset(count);
+                    break;
+                }
+                else
+                {
+                    count -= length;
+                    res = res.GetPositionAtOffset(length);
+                    res = res.GetInsertionPosition(LogicalDirection.Forward);
+                    if (res.GetPointerContext(LogicalDirection.Forward) == TextPointerContext.ElementEnd)
+                    {
+                        res = res.GetPositionAtOffset(2);
+                    }
+                    if (res != null && lastPara != res.Paragraph)
+                    {
+                        // counter the countdown that happens if we're stood just outside a paragraph
+                        if (lastPara == null)
+                        {
+                            count += 2;
+                        }
+                        lastPara = res.Paragraph;
+                        count -= 2;
+                    }
+                }
+            }
+            if (res != null && !res.IsAtInsertionPosition)
+            {
+                res = res.GetNextInsertionPosition(LogicalDirection.Forward);
+            }
+            return res;
+        }
+
+        public IEnumerable<String> GetVoices()
+        {
+            foreach (var voice in _synth.GetInstalledVoices())
+            {
+                yield return voice.VoiceInfo.Name;
+            }
+            //return _synth.GetInstalledVoices().SelectMany(s => s.VoiceInfo.Name );
+        }
+
+        public string GetVoice()
+        {
+
+            return _synth.Voice.Name;
+        }
+        public string SetVoice(String voice)
+        {
+            try
+            {
+                _synth.SpeakAsyncCancelAll();
+                _synth.SelectVoice(voice);
+
+                OnPropertyChanged("Voice");
+            }
+            catch (Exception)
+            {
+            
+            }
+            return _synth.Voice.Name;
+        }
+
+        public void SetRate(int value)
+        {
+            _synth.Rate = value;
+            OnPropertyChanged("Rate");
+        }
+        public int GetRate()
+        {
+            return _synth.Rate;
+        }
+
+        public void StartReading(TextPointer startingPoint)
+        {
+            _synth.SpeakAsyncCancelAll();
+            _synth.SpeakAsync(richTextBoxToPromt(startingPoint));
+            startedReading = startingPoint;
+            State = ReaderState.StartedSpeaking;
+        }
+        public void StopReading()
+        {
+            _synth.SpeakAsyncCancelAll();
+        }
+
+        public void PauseReading()
+        {
+            if (_synth.State == SynthesizerState.Speaking)
+            {
+                _synth.Pause();
+                State = ReaderState.Paused;
+            }
+        }
+        public void ResumeReading()
+        {
+            if (_synth.State==SynthesizerState.Paused )
+            {
+                _synth.Resume();
+                State = ReaderState.Speaking;
+            }
+        }
+
+        TextPointer startedReading;
+        private String richTextBoxToPromt(TextPointer startingPoint)
+        {
+            var res = new StringBuilder();
+        
+            TextRange text = new TextRange(startingPoint, startingPoint.DocumentEnd);
+            res.Append(text.Text);
+            
+            return res.ToString();
+        }
+
+        public ReaderState State
+        {
+            get { return _state; }
+            private set{
+                _state = value;
+                OnPropertyChanged("State");
+            }
+        }
+
+        public TextRange ReadText
+        {
+            get { return _readText; }
+            protected set
+            {
+                _readText = value;
+                // Call OnPropertyChanged whenever the property is updated
+                OnPropertyChanged("ReadText");
+            }
+        }
+
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this,new PropertyChangedEventArgs(name));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private TextRange _readText;
+        private ReaderState _state;
+    }
+
+    enum ReaderState
+    {
+        StartedSpeaking,
+        Speaking,
+        Paused,
+        NotSpeaking
+    }
+}
