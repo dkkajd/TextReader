aea95b1f100f94d7690121ac9a4ba89239e508b4
dkkajf_cp <SND\dkkajf_cp>
2011-03-23 11:50:15 +0000

    Removed an warning, still not sure exactly what to do about versioning.

--- TextReader/TextReader/Properties/AssemblyInfo.cs
+++ TextReader/TextReader/Properties/AssemblyInfo.cs
@@ -52,6 +52,6 @@ using System.Windows;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.1.*")]
-[assembly: AssemblyFileVersion("1.1.*")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
 
 [assembly: System.Runtime.CompilerServices.InternalsVisibleTo("TestProject1")]
